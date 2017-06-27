4cd8eacfb83d4ce3c5aa27b6a4779d4f8128a360
Kristof Boucher Charbonneau <kristof.b.c@me.com>
2017-06-27 21:40:07 +0900

    Removing default Slim page

--- src/routes.php
+++ src/routes.php
@@ -2,11 +2,8 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->response->withJson(["data" => []]);
 });
 
 $app->add(function ($req, $res, $next) {
